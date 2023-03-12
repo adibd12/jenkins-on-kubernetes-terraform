a82abab43f1f70f5a2c656dd0a8199f3be704ece
adibd12 <58192161+adibd12@users.noreply.github.com>
2023-03-12 22:54:30 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+cat <<EOF
+    ### Install Dependencys
+EOF
+sleep 5
+source dependency.sh
+
+
+             echo      "----- ............................. -----"
+             echo           "---  LOAD-TERRAFORM-FILES  ---"
+             echo      "----- ............................. -----"
+
+sleep 5         
+terraform init && terraform plan
+terraform apply -auto-approve
+sleep 10 && kubectl get pods -A && sleep 5
